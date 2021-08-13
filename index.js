e782d13c17bb22f4546d3546bfb510b160c6f642
luzelenie <luzelenie1205@gmail.com>
2021-08-13 12:44:08 -0400

    commit tree 95fecf73bb128d7caba0f60cedb27de888efdfe0

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+
+function findMatching (drivers, name) {
+    return drivers.filter(names => names === name.toLowerCase() || names === name)
+}
+function fuzzyMatch(drivers,letter){
+
+return drivers.filter(match => match.slice(0,letter.length) === letter) 
+}
+
+function matchName(drivers,name){
+    return drivers.filter(driver =>driver.name === name)
+    }
